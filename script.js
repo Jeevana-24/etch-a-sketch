0fee716deb1c45d87d0a6eb7a1214af875c60381
Jeevana <109099313+Jeevana-24@users.noreply.github.com>
2024-01-26 13:10:25 -0800

    final

---
+++ script.js
@@ -0,0 +1,25 @@
+const b=document.querySelector('body')
+
+const para=document.createElement('p')
+para.textContent="Draw Here"
+b.appendChild(para)
+
+const cont=document.createElement("div")
+for (let i=0;i<64;i++){
+    const grid=document.createElement('div')
+    grid.classList.add("row")
+    for(let i=0;i<64;i++){
+        const grids=document.createElement('button')
+        grids.classList.add("col")
+        grids.style.backgroundColor="white"
+        grid.appendChild(grids)
+    }
+    cont.appendChild(grid)
+}
+b.appendChild(cont)
+const sel=document.querySelectorAll("button")
+for(let i=0;i<64*64;i++){
+    sel[i].addEventListener("mouseover", () => {
+        sel[i].style.backgroundColor = "black";
+    });
+    }
